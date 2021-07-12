63c638c1052b64f5dcc9a2efb24e624a84ea1600
SoRA_X7 <galaxycomet2002@gmail.com>
2021-07-12 15:21:10 +0900

    Replace skybox from asset store with a simple picture

--- Assets/Quadspace/TBP/Randomizer/SevenBagRandomizerStart.cs
+++ Assets/Quadspace/TBP/Randomizer/SevenBagRandomizerStart.cs
@@ -4,6 +4,7 @@ namespace Quadspace.TBP.Randomizer {
     public class SevenBagRandomizerStart {
         public string type = "seven_bag";
         public List<string> bag_state;
+
         public SevenBagRandomizerStart(List<string> bagState) {
             bag_state = bagState;
         }
